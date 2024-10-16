1d2b4929a04df36f4584f7554c8addafba5b1200
Zairen <Zairen.Z@foxmail.com>
2024-10-15 23:02:29 -0700

    update q1 q2

---
+++ bioinformatics_project/scripts/generate_fasta.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import os
+
+random.seed(123)
+line_len = 80
+total_len = 10**6  # Use ** for exponentiation
+line_num = total_len // line_len  # 12500
+
+output = ""
+for i in range(line_num):
+    output += "".join(random.choices("ACGT", k=line_len)) + "\n"
+
+output_addr = os.path.join(os.path.dirname(__file__), "../data/random_sequence.fasta")
+with open(output_addr, "w") as f:
+    f.write(output)
+
+print(f"Random sequence has been generated and saved to {output_addr}")
---
+++ setup_project.sh
@@ -0,0 +1,7 @@
+mkdir -p ./bioinformatics_project/data ./bioinformatics_project/scripts ./bioinformatics_project/results
+touch ./bioinformatics_project/scripts/generate_fasta.py
+touch ./bioinformatics_project/scripts/dna_operations.py
+touch ./bioinformatics_project/scripts/find_cutsites.py
+touch ./bioinformatics_project/results/cutsite_summary.txt
+touch ./bioinformatics_project/data/random_sequence.fasta
+echo "This is the readme file for this little project." > ./bioinformatics_project/README.md
